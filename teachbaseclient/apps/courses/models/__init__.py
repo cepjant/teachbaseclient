8abf5657ba968890e1a519e570805aa5387db2a4
cepjant <vernigor191919@gmail.com>
2023-05-08 04:35:04 +0300

    добавлены модели для приложения Courses

---
+++ teachbaseclient/apps/courses/models/__init__.py
@@ -0,0 +1,11 @@
+from .author import Author
+from .course import Course  #
+from .coursetype import CourseType #
+from .material import Material
+from .owner import Owner
+from .poll import Poll
+from .quizz import Quizz
+from .scorm_package import ScormPackage
+from .section import Section
+from .task import Task
+from .xapi_package import XAPIPackage
